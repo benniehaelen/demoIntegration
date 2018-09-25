1e2588644c031349c530cac45b11cda846c9cf75
Nathan Parson Koroso <nathanielyhvh@gmail.com>
2018-09-25 16:34:14 -0700

    Add missing files

--- dist/apps/web-dynocard/main.js
+++ dist/apps/web-dynocard/main.js
@@ -1149,7 +1149,7 @@ exports.ChartsModule = ChartsModule;
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<h3>{{text}}</h3>\n<div>{{chartData | json}}</div>\n"
+module.exports = "<h3>{{text}} - API loaded data</h3>\n<div>{{chartData | json}}</div>\n\n<h3>Sample Multi-Line Chart</h3>\n\n<svg width=\"960\" height=\"500\"></svg>\n"
 
 /***/ }),
 
@@ -1237,9 +1237,10 @@ var DynoCardComponent = /** @class */ (function (_super) {
                     case 0: return [4 /*yield*/, this.dataService.get(this.urlManagingService.getDynoCardSampleData).toPromise()
                             .then(function (response) {
                             _this.chartData = response;
+                            _this.renderChart();
                         })
                             .catch(function (error) {
-                            console.log('DynoCardComponent.loadChartData() Error:', error.displayMessage);
+                            console.log('DynoCardComponent.loadChartData() Error:', error);
                             return error;
                         })];
                     case 1:
@@ -1249,10 +1250,99 @@ var DynoCardComponent = /** @class */ (function (_super) {
             });
         });
     };
+    DynoCardComponent.prototype.renderChart = function () {
+        var svg = d3.select("svg"), margin = { top: 20, right: 80, bottom: 30, left: 50 }, width = svg.attr("width") - margin.left - margin.right, height = svg.attr("height") - margin.top - margin.bottom, g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var parseTime = d3.timeParse("%Y%m%d");
+        var x = d3.scaleTime().range([0, width]), y = d3.scaleLinear().range([height, 0]), z = d3.scaleOrdinal(d3.schemeCategory10);
+        var line = d3.line()
+            .curve(d3.curveBasis)
+            .x(function (d) {
+            return x(d.date);
+        })
+            .y(function (d) {
+            return y(d.temperature);
+        });
+        d3.tsv("assets/data.tsv", type, function (error, data) {
+            if (error)
+                throw error;
+            var cities = data.columns.slice(1).map(function (id) {
+                return {
+                    id: id,
+                    values: data.map(function (d) {
+                        return { date: d.date, temperature: d[id] };
+                    })
+                };
+            });
+            x.domain(d3.extent(data, function (d) {
+                return d.date;
+            }));
+            y.domain([
+                d3.min(cities, function (c) {
+                    return d3.min(c.values, function (d) {
+                        return d.temperature;
+                    });
+                }),
+                d3.max(cities, function (c) {
+                    return d3.max(c.values, function (d) {
+                        return d.temperature;
+                    });
+                })
+            ]);
+            z.domain(cities.map(function (c) {
+                return c.id;
+            }));
+            g.append("g")
+                .attr("class", "axis axis--x")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+            g.append("g")
+                .attr("class", "axis axis--y")
+                .call(d3.axisLeft(y))
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "0.71em")
+                .attr("fill", "#000")
+                .text("Temperature, ºF");
+            var city = g.selectAll(".city")
+                .data(cities)
+                .enter().append("g")
+                .attr("class", "city");
+            city.append("path")
+                .attr("class", "line")
+                .attr("d", function (d) {
+                return line(d.values);
+            })
+                .style("stroke", function (d) {
+                return z(d.id);
+            });
+            city.append("text")
+                .datum(function (d) {
+                return { id: d.id, value: d.values[d.values.length - 1] };
+            })
+                .attr("transform", function (d) {
+                return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")";
+            })
+                .attr("x", 3)
+                .attr("dy", "0.35em")
+                .style("font", "10px sans-serif")
+                .text(function (d) {
+                return d.id;
+            });
+        });
+        function type(d, _, columns) {
+            d.date = parseTime(d.date);
+            for (var i = 1, n = columns.length, c = void 0; i < n; ++i)
+                d[c = columns[i]] = +d[c];
+            return d;
+        }
+    };
     DynoCardComponent = __decorate([
         core_1.Component({
             selector: 'dynocard-chart',
-            template: __webpack_require__(/*! ./dynocard.component.html */ "../../xplat/web/features/charts/components/charts/dynocard.component.html")
+            template: __webpack_require__(/*! ./dynocard.component.html */ "../../xplat/web/features/charts/components/charts/dynocard.component.html"),
+            styles: [__webpack_require__(/*! ./dynocard.style.css */ "../../xplat/web/features/charts/components/charts/dynocard.style.css")],
+            encapsulation: core_1.ViewEncapsulation.None
         }),
         __metadata("design:paramtypes", [core_2.DataService, core_2.UrlManagingService])
     ], DynoCardComponent);
@@ -1260,6 +1350,17 @@ var DynoCardComponent = /** @class */ (function (_super) {
 }(features_1.DynoCardBaseComponent));
 exports.DynoCardComponent = DynoCardComponent;
 
+
+/***/ }),
+
+/***/ "../../xplat/web/features/charts/components/charts/dynocard.style.css":
+/*!***************************************************************************************************************!*\
+  !*** /home/flytoy/DevSource/iot-edge-dynocard/xplat/web/features/charts/components/charts/dynocard.style.css ***!
+  \***************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ".axis--x path {\n  display: none;\n}\n\n.line {\n  fill: none;\n  stroke: steelblue;\n  stroke-width: 1.5px;\n}\n"
 
 /***/ }),
 
